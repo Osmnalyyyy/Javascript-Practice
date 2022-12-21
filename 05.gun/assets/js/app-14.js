fc94118b6d7e1e200acb01d4cd01df73e3170da3
Osmnalyyyy <108487432+Osmnalyyyy@users.noreply.github.com>
2022-12-21 20:59:33 +0300

    21.12.2022

---
+++ 05.gun/assets/js/app-14.js
@@ -0,0 +1,14 @@
+let image = document.querySelector(".resim");
+
+document.querySelector(".on").addEventListener("click", () => {
+  image.src = "assets/img/lamba_on.gif";
+});
+document.querySelector(".off").addEventListener("click", () => {
+  image.src = "assets/img/lamba_off.gif";
+});
+image.addEventListener("mouseover", () => {
+  image.src = "assets/img/lamba_on.gif";
+});
+image.addEventListener("mouseout", () => {
+  image.src = "assets/img/lamba_off.gif";
+});
